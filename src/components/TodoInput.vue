7a9f207161935c352badfe78167fda72f33ce730
sjkim <sunjoong91@nate.com>
2021-05-15 20:03:42 +0900

    TodoInputVue

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,7 +1,10 @@
 <template>
-  <div>
-      <input type="text" v-model="newTodoItem" v-id="asdf">
-      <button v-on:click="addTodo">추가</button>
+  <div class="inputBox shadow">
+        <input type="text" v-model="newTodoItem" placeholder="내용을 입력하세요." v-on:keyup.enter="addTodo">
+      <!--<button v-on:click="addTodo">추가</button>-->
+        <span class="addContainer" v-on:click="addTodo">
+            <i class="addBtn fas fa-plus" aria-hidden="true"></i>
+        </span>
   </div>
 </template>
 
@@ -14,11 +17,45 @@ export default{
     },
     methods:{
         addTodo(){
-            console.log(this.newTodoItem)
+            //console.log(this.newTodoItem)
+            var value = this.newTodoItem;
+            if(value != ""){
+                //var value = this.newTodoItem && this.newTodoItem.trim();
+                localStorage.setItem(value, value);
+                this.clearInput();
+            }
+        },
+        clearInput(){
+            this.newTodoItem = '';
         }
     }
 }
 </script>
 
-<style>
+<style scoped>
+    input:focus{
+        outline:none;
+    }
+    .inputBox{
+        background: white;
+        height: 50px;
+        line-height: 50px;
+        border-radius: 5px;
+    }
+    .inputBox input{
+        border-style: none;
+        font-size:0.9rem;
+    }
+    .addContainer{
+        float: right;
+        background: linear-gradient(to right, #6478FB, #8763FB);
+        display: black;
+        width:3rem;
+        border-radius:0 5px 5px 0;
+    }
+    .addBtn{
+        color:white;
+        vertical-align:middle;
+    }
+
 </style>
